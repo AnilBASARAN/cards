4b3e0c73d9d751101b791d1c8f37123d33ca6942
AnilBASARAN <nlbsrn@gmail.com>
2022-03-22 04:09:38 +0200

    first attempt

---
+++ main.js
@@ -0,0 +1,79 @@
+
+
+let deckID = ''
+
+let url = "https://deckofcardsapi.com/api/deck/ssxjcntumfza/draw/?count=2"
+fetch(url) 
+    .then(res => res.json())
+    .then(data =>{
+
+       
+        console.log(data)
+        console.log(data.cards)
+
+
+        val1 = (data.cards[0].value)
+         document.querySelector('#player1_image').src = (data.cards[0].image)
+
+         val2 = (data.cards[1].value)
+
+        document.querySelector('#player2_image').src = (data.cards[1].image)
+
+       
+        
+       
+        function cardValue (vall) {
+            
+            if (vall === "ACE") {
+                return 14
+            } else if(vall === "KING") {
+                return 13
+            } else if (vall === "QUEEN"){
+                return 12
+            }else if(vall === "JACK"){
+                return 11
+            }else {
+                return vall;
+            }
+
+
+        }
+       
+       
+        let a = Number(cardValue(val1))
+        let b = Number(cardValue(val2))
+
+       
+        function val(x,y){
+           
+            if( x < y ){
+                document.querySelector('h3').innerText = "Player 2 Won !"
+            }else if( y < x) {
+             document.querySelector('h3').innerText = "Player 1 Won !"
+            } else{
+                document.querySelector('h3').innerText = "WAR !"
+            }
+        }
+    
+        
+        
+        
+        
+        val(a,b)
+        
+        
+
+       
+
+
+
+
+
+
+
+
+       
+    });
+
+
+    
